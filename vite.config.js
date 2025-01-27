3935f7983ab34d7ae80903b910429554760fbe1a
Aisha-hash <175808683+Aisha-hash@users.noreply.github.com>
2025-01-27 22:26:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Define the process.env.BACKEND_URL which can be used in your code
-    'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'http://localhost:3001'),
+    // Define the VITE_BACKEND_URL which can be used in your code
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL || 'http://localhost:3001'),
   }
 })
