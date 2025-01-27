cc112dd7d71aa37b7359e97da6ae8e69f0e6a0ee
Aisha-hash <175808683+Aisha-hash@users.noreply.github.com>
2025-01-27 22:10:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Define the process.env.BACKEND_URL which can be used in your code
+    'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'http://localhost:3001'),
+  }
 })
